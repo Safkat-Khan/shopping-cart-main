34a7f725f539c52aab834fbcee9be3b89bd6cb81
Safkat khan Tafsir <safkatkhan420@gmail.com>
2022-09-30 16:02:13 +0600

    update

--- shopping-cart-main/script.js
+++ shopping-cart-main/script.js
@@ -1,42 +1,66 @@
-// Increasing Value
+function UpdatePhoneNumber(isIncrease){
+        const prv_Value = document.getElementById('phnInputNumber');
+        const prv_Valueout = prv_Value.value;
+        const the_Value = parseInt(prv_Valueout);
+
+        let newphnnumber;
+
+        if(isIncrease === true){
+            newphnnumber = the_Value + 1;
+        }
+        else{
+            newphnnumber = the_Value - 1; 
+        }
+        prv_Value.value = newphnnumber;
+    }
+
 document.getElementById('phnPlus').addEventListener('click',function(){
-    const prv_Value = document.getElementById('phnInputNumber');
-    const prv_Valueout = prv_Value.value;
-    const the_Value = parseInt(prv_Valueout);
-    const new_Value = the_Value + 1;
-    prv_Value.value = new_Value;
+    UpdatePhoneNumber(true);
 })
-// Discreasing Value
+
 document.getElementById('phn-neg').addEventListener('click',function(){
-    const onevalue = document.getElementById('phnInputNumber');
-    const onevalueString = onevalue.value;
-    const onevalueInt = parseInt(onevalueString);
-    const newvalue = onevalueInt - 1;
-    onevalue.value = newvalue;
+    UpdatePhoneNumber(false);
 })
 
 //const onePhonePrice = document.getElementById('phn-price');
 
-document.getElementById('casePlus').addEventListener('click',function(){
+function CaseValue(IsIncrease){
     const caseValue = document.getElementById('caseValue');
     const caseString = caseValue.value;
     const caseInt = parseInt(caseString);
-    const upValue = caseInt + 1;
-    caseValue.value = upValue;
-})
 
-document.getElementById('downCase').addEventListener('click',function(){
-    const caseValueDown = document.getElementById('caseValue');
-    const caseDownString = caseValueDown.value;
-    const caseDownInt = parseInt(caseDownString);
-    const downCase = caseDownInt - 1;
-    caseValueDown.value = downCase;
-})
+    let newCaseNumber ;
 
-    const phnPrice = document.getElementById('phn-price');
-    const phnPriceString = phnPrice.value;
-    const phnInt = parseInt(phnPriceString);
-    const upPrice = phnInt + 1219;
+    if(IsIncrease === true){
+        newCaseNumber = caseInt + 1;
+    }
+    else{
+        newCaseNumber = caseInt - 1;
+    }
+    caseValue.value = newCaseNumber;
+
+    return newCaseNumber
+}
+    document.getElementById('casePlus').addEventListener('click',function(){
+       const newCaseNumber =  CaseValue( true);
+
+       const caseTotalPrice = newCaseNumber * 59;
+       const caseTotslElement = document.getElementById('caseTotal');
+       caseTotslElement.innerText = caseTotalPrice
+
+    })
+
+    document.getElementById('downCase').addEventListener('click',function(){
+    
+        const caseTotalPrice = newCaseNumber * 59;
+        const caseTotslElement = document.getElementById('caseTotal');
+        caseTotslElement.innerText = caseTotalPrice
+    })
 
 
-console.log();
+
+
+
+
+
+         console.log();
